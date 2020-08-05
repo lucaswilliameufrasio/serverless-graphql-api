ee7a8dc5370a2a759f7a8a3c4cbb7d3db1d39151
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-05 05:13:40 -0300

    chore: update bundle folder

--- functions/bundle/index.js
+++ functions/bundle/index.js
@@ -1,7 +1,22 @@
 const { createLocalServer } = require('./server')
+const mongoose = require('mongoose')
 
 const server = createLocalServer()
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/lambda'
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+
+db.once('open', () => {
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+  })
 })
--- functions/bundle/server.js
+++ functions/bundle/server.js
@@ -1,15 +1,89 @@
+require('dotenv').config()
+
 const ApolloServer = require('apollo-server').ApolloServer
 const ApolloServerLambda = require('apollo-server-lambda').ApolloServer
 const { gql } = require('apollo-server-lambda')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const { decodedToken } = require('./decodedToken')
+
+const UserSchema = require('./User')
 
 const typeDefs = gql`
+  type User {
+    name: String!
+    email: String!
+    password: String!
+    id: String
+  }
+
+  input UserCreateInput {
+    email: String!
+    name: String!
+    password: String!
+  }
+
+  input UserLoginInput {
+    email: String!
+    password: String!
+  }
+
+  type AuthPayload {
+    token: String!
+  }
+
+  type Mutation {
+    signupUser(data: UserCreateInput!): AuthPayload!
+    loginUser(data: UserLoginInput!): AuthPayload!
+  }
+
   type Query {
     hello: String
+    users: [User]
   }
 `
+
 const resolvers = {
   Query: {
-    hello: () => "Hi, i'm @lucaswilliameufrasio"
+    hello: () => "Hi, i'm @lucaswilliameufrasio",
+    users: async (root, args, { UserSchema, req }, info) => {
+      const decoded = decodedToken(req)
+
+      return UserSchema.find().populate()
+    }
+  },
+
+  Mutation: {
+    signupUser: async (root, args, { UserSchema }, info) => {
+      const {
+        data: { email, name, password }
+      } = args
+
+      const hashedPassword = bcrypt.hashSync(password, 3)
+      const newUser = await UserSchema.create({
+        email,
+        name,
+        password: hashedPassword
+      })
+
+      return { token: jwt.sign(newUser.email, 'supersecret') }
+    },
+
+    loginUser: async (root, args, { UserSchema }, info) => {
+      const {
+        data: { email, password }
+      } = args
+
+      const theUser = await UserSchema.findOne({ email })
+
+      if (!theUser) throw new Error('Unable to Login')
+      const isMatch = bcrypt.compareSync(password, theUser.password)
+
+      if (!isMatch) throw new Error('Unable to Login')
+
+      return { token: jwt.sign(theUser.email, 'supersecret') }
+    }
   }
 }
 
@@ -17,6 +91,10 @@ function createLambdaServer() {
   return new ApolloServerLambda({
     typeDefs,
     resolvers,
+    context: (req) => ({
+      UserSchema,
+      req
+    }),
     introspection: true,
     playground: true
   })
@@ -26,6 +104,10 @@ function createLocalServer() {
   return new ApolloServer({
     typeDefs,
     resolvers,
+    context: (req) => ({
+      UserSchema,
+      req
+    }),
     introspection: true,
     playground: true
   })
